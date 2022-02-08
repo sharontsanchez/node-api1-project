82ace832bd3397cc61fcf62108a669a4e527b3b6
Sharon Sanchez <sstharakan@gmail.com>
2022-02-08 14:32:39 -0600

    created new user via POST

--- api/server.js
+++ api/server.js
@@ -4,8 +4,34 @@ const User = require('./users/model')
 
 
 const server = express()
+
+// it doesnt know how to parse from request
+server.use(express.json())
+
+
+// create new user 
 // [POST] /api/users
 
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio){
+        res.status(400).json({
+            message: 'Please provide name and bio for the user',
+        })    
+    } else {
+      User.insert(user)
+        .then(createdUser => {
+                res.status(201).json(createdUser)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
+})
 
 
 // [GET] /api/users
