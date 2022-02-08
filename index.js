38400d5914e8d8290d3b18bf3ef875a8a11888f9
Sharon Sanchez <sstharakan@gmail.com>
2022-02-08 11:50:24 -0600

    build server and ttesting an endpoint to see if it wworks

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+// console.log('hello world')
+
+server.listen(port, () => {
+    console.log('listening on', port)
+})
