84076aaa649317f71544d3900b66bebc083e2bb1
Sharon Sanchez <sstharakan@gmail.com>
2022-02-08 15:08:56 -0600

    update user information via PUT

--- api/server.js
+++ api/server.js
@@ -98,19 +98,34 @@ server.delete('/api/users/:id', async (req, res) => {
 
 // [PUT] /api/users/:id
 // update and 
-// server.get('/api/users', (req, res) => {
-//     User.find()
-//       .then(users => {
-//           res.json(users)
-//       })
-//       .catch( err => {
-//           res.status(500).json({
-//               message: 'The user information could not be modified',
-//               err: err.message,
-//               stack: err.stack,
-//           })
-//       })
-//   })
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser){
+            res.status(404).json({
+                message:'The user with the specified ID does not exist'
+            })
+        } else {
+            if (!req.body.name || !req.body.bio){
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                })
+            } else {
+                const updatedUser = await User.update(
+                    req.params.id, 
+                    req.body,
+                    )
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+  })
 
 
 // build an endpoint to test if app is working
